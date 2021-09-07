46d128ce4be46487e983cb39a633979dc16fb285
DongHwiCho <junejoe97@inha.edu>
2021-09-07 17:30:13 +0900

    OpticalFlow Add

--- FinalExam_DongHwi.py
+++ FinalExam_DongHwi.py
@@ -18,6 +18,7 @@ bg_frame = 0
 bg_count = 0
 acc_bgr = np.zeros(shape=(height, width, 3), dtype=np.float32)
 bkg_sub = cv2.createBackgroundSubtractorMOG2()
+mask_f = np.zeros(shape=(height, width, 3), dtype=np.uint8)
 
 #Run Video
 while True:
@@ -41,7 +42,7 @@ while True:
     C_Hist = cv2.calcHist([imgC], [0], None, [256], [0, 256]) #Current Frame Histogram
     if not current_frame == 0:
         diff_Hist = C_Hist - P_Hist #Histogram substract
-        # print('current_frame = {}, diff = {}'.format(current_frame, np.abs(diff_Hist).sum()))
+        print('current_frame = {}, diff = {}'.format(current_frame, np.abs(diff_Hist).sum()))
 
         #diff_Hist over 100000 is shot changed
         if np.abs(diff_Hist).sum() > 100000:
@@ -52,6 +53,7 @@ while True:
             cv2.imwrite('./Background{}.png'.format(bg_count), cv2.convertScaleAbs(acc_bgr/bg_frame))
             bkg_sub = cv2.createBackgroundSubtractorMOG2()
             acc_bgr = np.zeros(shape=(height, width, 3), dtype=np.float32)
+            mask_f = np.zeros(shape=(height, width, 3), dtype=np.uint8)
             bg_frame = 0
             bg_count += 1
         else:
@@ -63,20 +65,23 @@ while True:
             cv2.accumulate(cv2.bitwise_and(frame, frame, mask=bImage), acc_bgr)
 
             #Optical Flow
-            if current_frame != 0:
-                contours, hierachy = cv2.findContours(cv2.bitwise_not(bImage), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                flow = cv2.calcOpticalFlowFarneback(imgP, imgC, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-                for cnt in contours:
-                    area = cv2.contourArea(cnt)
-                    if area > 100:
-                        x, y, width, height = cv2.boundingRect(cnt)
-                        cv2.rectangle(frame_c, (x, y), (x+width, y+height), (0, 0, 255), 2)
-                
+            if bg_frame != 0:
+                corners_current = cv2.goodFeaturesToTrack(imgC, maxCorners=100, qualityLevel=0.05, minDistance=10)
+                p1, st, err = cv2.calcOpticalFlowPyrLK(imgP, imgC, corners_current, None, winSize=(5, 5), maxLevel=3, criteria=(cv2.TERM_CRITERIA_MAX_ITER+cv2.TERM_CRITERIA_EPS, 10, 0.01))
+                good_new = p1[st==1]
+                good_old = corners_current[st==1]
+                for i,(new,old) in enumerate(zip(good_new, good_old)):
+                    a,b = new.ravel()
+                    c,d = old.ravel()
+                    mask_f = cv2.line(mask_f, (int(a), int(b)), (int(c), int(d)), (255, 255, 255), 2)
+                    frame_c = cv2.circle(frame_c, (int(a), int(b)), 5, (255, 0, 0),-1)
+                    frame_c = cv2.add(frame_c, mask_f)
+
     cv2.imshow('frame', frame_c)
     current_frame += 1
     imgP = imgC.copy() #Previous Frame
     P_Hist = C_Hist #Previous Frame Histogram
-    key = cv2.waitKey(1)
+    key = cv2.waitKey(10)
     if key == 27:
         break
 
